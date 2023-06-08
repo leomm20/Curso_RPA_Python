c09ce6621cd648d5e453314f08f814f86c3cd2cd
leomm20 <leonardo.maggiotti@gmail.com>
2023-06-08 18:18:22 -0300

    1

--- src/utils.py
+++ src/utils.py
@@ -24,9 +24,26 @@ def write(text: str):
     r.hotkey('ctrl', 'v')
 
 
-def sleep(time_secs=1.0):
+def sleep(time_secs: float = 1.0):
     """
     Se utiliza para generar una pausa, dando tiempo al sistema a que finalice algún proceso (ej. cargar una página web)
     :param time_secs: 1 segundo como default
     """
     time.sleep(time_secs)
+
+
+def pn(posicion_referencia_x: int, posicion_referencia_y: int) -> tuple:
+
+    posicion_referencia = posicion_referencia_x, posicion_referencia_y
+    # Obtener la posición relativa del ícono en una resolución de referencia
+    resolucion_referencia = (1920, 1080)
+
+    # Obtener la resolución actual
+    resolucion_actual = r.size()
+
+    # Calcular el factor de escala para normalizar las coordenadas
+    escala_x = resolucion_actual[0] / resolucion_referencia[0]
+    escala_y = resolucion_actual[1] / resolucion_referencia[1]
+
+    # Calcular las coordenadas normalizadas y retornar
+    return int(posicion_referencia[0] * escala_x), int(posicion_referencia[1] * escala_y)
